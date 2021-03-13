8d4d318b9453c398a050aefd9face95f81c8cc33
mohammad610 <79204128+mohammad610@users.noreply.github.com>
2021-03-13 22:12:36 +0330

    Delete contact.php

--- contact.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbc = new mysqli('sql201.b6b.ir', 'b6bi_27916015', 'Qwertyui0913', 'b6bi_27916015_ertebat'); // mysqli_connect
-$sql = "INSERT INTO message(name,email,title, body)
-		VALUES('{$_POST['user']}','{$_POST['email']}','{$_POST['title']}', '{$_POST['message']}')";
-$result = $dbc -> query( $sql ); // mysqli_query
-$dbc -> close(); //mysqli_close
-// C. success message
-echo 'پیام با موفقیت ثبت شد';
-?>
