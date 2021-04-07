52b4ec61a16cbd27638e6ed5b106ca299d00f4f8
mohammad610 <79204128+mohammad610@users.noreply.github.com>
2021-04-07 21:48:08 +0430

    Delete public directory

--- public/adduser.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbc = new mysqli('sql201.b6b.ir', 'b6bi_27916015', 'Qwertyui0913', 'b6bi_27916015_user'); // mysqli_connect
-$sql = "INSERT INTO user(name,famil,email)
-		VALUES('{$_POST['user']}','{$_POST['famil']}','{$_POST['email']}')";
-$result = $dbc -> query( $sql ); // mysqli_query
-$dbc -> close(); //mysqli_close
-// C. success message
-echo 'پیام با موفقیت ثبت شد';
-?>
